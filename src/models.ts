5465fb00a80de6a9532a2f307026adcb449045fa
Sirius <boukhanoucheayoubpro@gmail.com>
2026-02-23 22:09:31 +0100

    Fix Models

--- src/models.ts
+++
@@ -1,5 +0,0 @@
-export const MODELS = [
-    { name: "Immeuble résidentiel", file: "building_A.glb" },
-    { name: "Maison petite", file: "building_B.glb" },
-    { name: "Route - angle", file: "building_C.glb" }
-];
